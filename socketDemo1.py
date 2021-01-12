fe05b61dbfcc7c65dab15f26b17565a39dadbde5
yanerfree <1542326207@qq.com>
2021-01-12 14:12:37 +0800

    just a test

---
+++ socketDemo1.py
@@ -0,0 +1,19 @@
+import socket
+
+
+sock=socket.socket()
+sock.bind(("127.0.0.1",8808))
+sock.listen(5)
+
+while 1:
+    print("server waiting.....")
+    conn,addr=sock.accept()
+    data=conn.recv(1024)
+    print("data",data)
+
+    # 读取html文件
+    with open("login.html","rb") as f:
+        data=f.read()
+
+    conn.send((b"HTTP/1.1 200 OK\r\n\r\n%s"%data))
+    conn.close()
